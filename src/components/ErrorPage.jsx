b0ad84c34bcbd2751dc8eedb6bdf135044a8cf2a
mahdee18 <mahdeec10@gmail.com>
2024-06-03 20:08:17 +0600

    Added Error Page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,428 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ShapeOne = () => {
+	return (
+		<svg
+			className="absolute top-0 right-0 hidden sm:block m-4 -z-[1]"
+			xmlns="http://www.w3.org/2000/svg"
+			width="230"
+			height="76"
+			viewBox="0 0 230 76"
+			fill="none"
+		>
+			<path
+				d="M34.0747 18.8387H12.9956V39.9179H34.0747V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M73.0527 18.8387H51.9736V39.9179H73.0527V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M112.048 18.8387H90.9692V39.9179H112.048V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M151.027 18.8387H129.948V39.9179H151.027V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M190.004 18.8387H168.925V39.9179H190.004V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M229 18.8387H207.92V39.9179H229V18.8387Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M34.0747 53.2292H12.9956V74.3084H34.0747V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M73.0527 53.2292H51.9736V74.3084H73.0527V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M112.048 53.2292H90.9692V74.3084H112.048V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M151.027 53.2292H129.948V74.3084H151.027V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M190.004 53.2292H168.925V74.3084H190.004V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M229 53.2292H207.92V74.3084H229V53.2292Z"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+			></path>
+			<path
+				d="M12.9956 1.46106H34.0573"
+				stroke="#8BD2BD"
+				strokeWidth="2"
+				strokeMiterlimit="10"
+				strokeLinecap="square"
+			></path>
+		</svg>
+	);
+};
+
+const ShapeTwo = () => {
+	return (
+		<svg
+			className="absolute bottom-0 left-0 hidden sm:block m-4 -z-[1]"
+			xmlns="http://www.w3.org/2000/svg"
+			width="271"
+			height="156"
+			viewBox="0 0 271 156"
+			fill="none"
+		>
+			<path
+				d="M231.808 302.075L229.912 300.652C255.143 268.138 268.483 229.297 268.483 188.31C268.483 124.815 235.935 66.3996 181.415 32.0468L182.709 30.0762C237.921 64.845 270.87 124.005 270.87 188.31C270.87 229.8 257.351 269.145 231.808 302.075Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M79.4461 376.188C-26.1147 376.188 -112 291.893 -112 188.287C-112 84.6815 -26.1147 0.386719 79.4461 0.386719V2.72944C-24.7985 2.72944 -109.591 85.9733 -109.591 188.266C-109.591 290.58 -24.7762 373.802 79.4461 373.802C110.945 373.802 142.131 366.051 169.614 351.382L170.752 353.44C142.912 368.35 111.324 376.188 79.4461 376.188Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M245.461 161.927C239.192 123.633 219.16 88.5358 189.022 63.116C158.572 37.4115 119.645 23.2675 79.4458 23.2675C46.9878 23.2675 15.4891 32.3539 -11.6596 49.5631C-43.2921 69.6187 -67.4293 99.8335 -79.6317 134.646L-84.1602 133.114C-71.6231 97.3156 -46.7945 66.2469 -14.2696 45.622C13.6598 27.9311 46.0509 18.582 79.4234 18.582C120.782 18.582 160.802 33.142 192.123 59.569C223.108 85.7113 243.721 121.816 250.168 161.204L245.461 161.927Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M79.4455 310.898C10.5588 310.898 -45.4785 255.898 -45.4785 188.287C-45.4785 160.919 -36.4884 135.039 -19.5122 113.451L-15.7198 116.319C-32.0492 137.098 -40.6823 161.97 -40.6823 188.287C-40.6823 253.293 13.2135 306.191 79.4455 306.191C91.2018 306.191 102.824 304.527 113.956 301.242L115.339 305.753C103.739 309.19 91.6479 310.898 79.4455 310.898Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M163.815 278.712L160.58 275.253C185.364 252.964 199.574 221.283 199.574 188.309C199.574 165.101 192.703 142.637 179.72 123.347L183.713 120.764C197.232 140.819 204.37 164.181 204.37 188.331C204.348 222.596 189.58 255.548 163.815 278.712Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M154.177 95.9796C132.739 79.2301 106.884 70.3847 79.4456 70.3847C55.3754 70.3847 32.1529 77.3472 12.2766 90.5279L9.59961 86.6306C30.2567 72.9245 54.4161 65.6992 79.4456 65.6992C108 65.6992 134.881 74.895 157.166 92.3232L154.177 95.9796Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M6.2988 260.124C-13.2429 240.945 -24.0176 215.437 -24.0176 188.31C-24.0176 132.325 22.3828 86.7617 79.4463 86.7617C136.488 86.7617 182.91 132.325 182.91 188.31H180.523C180.523 133.617 135.194 89.1263 79.4686 89.1263C23.7435 89.1263 -21.586 133.617 -21.586 188.31C-21.586 214.802 -11.079 239.719 8.03881 258.46L6.2988 260.124Z"
+				fill="#EAF7FA"
+			/>
+			<path
+				d="M79.4461 251.454C43.9543 251.454 15.0879 223.122 15.0879 188.287C15.0879 153.453 43.9543 125.121 79.4461 125.121V129.829C46.6089 129.829 19.8841 156.058 19.8841 188.287C19.8841 220.517 46.6089 246.747 79.4461 246.747C112.283 246.747 139.008 220.517 139.008 188.287H143.804C143.782 223.122 114.916 251.454 79.4461 251.454Z"
+				fill="#EAF7FA"
+			/>
+		</svg>
+	);
+};
+
+const ShapeThree = () => {
+	return (
+		<svg
+			className="absolute bottom-0 right-0 m-4 -z-[1]"
+			xmlns="http://www.w3.org/2000/svg"
+			width="130"
+			height="93"
+			viewBox="0 0 130 93"
+			fill="none"
+		>
+			<g opacity="0.5">
+				<path
+					d="M7.81955 92.1754V76.5363M15.6391 84.3558H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 53.6791V38.04M15.6391 45.8596H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 16.3859V0.746765M15.6391 8.56631H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 92.1754V76.5363M15.6391 84.3558H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 53.6791V38.04M15.6391 45.8596H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 16.3859V0.746765M15.6391 8.56631H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 92.1754V76.5363M51.7294 84.3558H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 53.6791V38.04M51.7294 45.8596H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 16.3859V0.746765M51.7294 8.56631H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 92.1754V76.5363M51.7294 84.3558H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 53.6791V38.04M51.7294 45.8596H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 16.3859V0.746765M51.7294 8.56631H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 53.6791V38.04M87.8193 45.8596H72.1802H87.8193Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 16.3859V0.746765M87.8193 8.56631H72.1802H87.8193Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 53.6791V38.04M87.8193 45.8596H72.1802H87.8193Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 16.3859V0.746765M87.8193 8.56631H72.1802H87.8193Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 53.6791V38.04M123.91 45.8596H108.271H123.91Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 16.3859V0.746765M123.91 8.56631H108.271H123.91Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 53.6791V38.04M123.91 45.8596H108.271H123.91Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 16.3859V0.746765M123.91 8.56631H108.271H123.91Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+			</g>
+		</svg>
+	);
+};
+
+const ShapeFour = () => {
+	return (
+		<svg
+			className="absolute left-0 top-0 m-4 -z-[1]"
+			xmlns="http://www.w3.org/2000/svg"
+			width="130"
+			height="93"
+			viewBox="0 0 130 93"
+			fill="none"
+		>
+			<g opacity="0.5">
+				<path
+					d="M7.81955 92.1754V76.5363M15.6391 84.3558H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 53.6791V38.04M15.6391 45.8596H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 16.3859V0.746765M15.6391 8.56631H0H15.6391Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 92.1754V76.5363M15.6391 84.3558H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 53.6791V38.04M15.6391 45.8596H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M7.81955 16.3859V0.746765M15.6391 8.56631H0H15.6391Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 92.1754V76.5363M51.7294 84.3558H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 53.6791V38.04M51.7294 45.8596H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 16.3859V0.746765M51.7294 8.56631H36.0903H51.7294Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 92.1754V76.5363M51.7294 84.3558H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 53.6791V38.04M51.7294 45.8596H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M43.9099 16.3859V0.746765M51.7294 8.56631H36.0903H51.7294Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 53.6791V38.04M87.8193 45.8596H72.1802H87.8193Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 16.3859V0.746765M87.8193 8.56631H72.1802H87.8193Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 53.6791V38.04M87.8193 45.8596H72.1802H87.8193Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M79.9997 16.3859V0.746765M87.8193 8.56631H72.1802H87.8193Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 53.6791V38.04M123.91 45.8596H108.271H123.91Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 16.3859V0.746765M123.91 8.56631H108.271H123.91Z"
+					stroke="#8BD2BD"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 53.6791V38.04M123.91 45.8596H108.271H123.91Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+				<path
+					d="M116.09 16.3859V0.746765M123.91 8.56631H108.271H123.91Z"
+					stroke="#5243C2"
+					strokeWidth="2"
+					strokeMiterlimit="10"
+				></path>
+			</g>
+		</svg>
+	);
+};
+
+export const ErrorPage = () => {
+	return (
+		<section className="ezy__httpcodes11 light py-48  bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white relative overflow-hidden z-[1]">
+			<ShapeOne />
+			<ShapeTwo />
+			<ShapeThree />
+			<ShapeFour />
+
+			<div className="container px-4 mx-auto">
+				<div className="flex w-full px-4 lg:px-12">
+					<div className="">
+						<h1 className="text-[55px] md:text-[70px] leading-none font-bold text-red-600 mb-2">
+							404
+						</h1>
+						<h4 className="text-[38px] font-bold opacity-80 mb-2">
+							Page Not Found
+						</h4>
+						<p className="text-lg opacity-80 mb-6">
+							Something went wrong, this page is broken.
+						</p>
+						<div className="flex">
+							<Link to={'/'} className="bg-[#1d293a] text-white hover:bg-opacity-90 py-3 px-4 font-bold rounded">
+								Return to Home
+							</Link>
+							<Link className="py-3 px-6 bg-transparent text-[white] font-bold border border-[#1d293a] hover:bg-[#1d293a] hover:text-white rounded ml-2">
+								Try Again
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
